46fd26b6b763055ac31a54b2276b678b756de7b6
Shado2107 <pd39almeida@outlook.com>
2023-08-14 18:03:18 +0000

    orders page

--- pages/customers.jsx
+++ pages/customers.jsx
@@ -11,7 +11,7 @@ const customers = () => {
                 <h2>Welcome back, Pavel</h2>
             </div>
             <div className='p-4'>
-                <div className='w-full m-auto border rounded-lg bg-white overflow-y-auto'>
+                <div className='w-full m-auto p-4 border rounded-lg bg-white overflow-y-auto'>
                     <div className="my-3 p-2 grid md:grid-cols-4 sm:grid-cols-3 grid-cols-2 items-center justify-between cursor-pointer">
                         <span>Name</span>
                         <span className="sm:text-left text-right">Email</span>
@@ -23,13 +23,18 @@ const customers = () => {
                             <li key={id} className="bg-gray-50 hover:bg-gray-100 rounded-lg my-3 p-2 grid md:grid-cols-4 sm:grid-cols-3 grid-cols-2 items-center justify-between cursor-pointer">
                                 <div className="flex items-center">
                                     <div className="bg-purple-100 p-3 rounded-lg">
-                                        <BsPersonFill className="bg-purple-800"/>
+                                        <BsPersonFill className="text-purple-800"/>
                                     </div>
                                     <p className='pl-4'> {order.name.first + ' ' + order.name.last} </p>
                                 </div>
                                 <p className="text-gray-600 sm:text-left text-right">
                                     {order.name.first}@gmail.com
                                 </p>
+                                <p className="hidden md:flex"> {order.date} </p>
+                                <div className="sm:flex hidden justify-between items-center">
+                                    <p> {order.method} </p>
+                                    <BsThreeDotsVertical/>
+                                </div>
                             </li>
                         ))}
                     </ul>
